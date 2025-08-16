e8727f02d3ba66d5402da5fa5dff9c4171e93c72
CrazyWoddMan <mishaocheret@gmail.com>
2025-08-16 23:39:04 +0200

    summary tooltip

--- src/main/java/crazywoddman/warium_create/WariumCreate.java
+++ src/main/java/crazywoddman/warium_create/WariumCreate.java
@@ -5,6 +5,11 @@ import crazywoddman.warium_create.block.WariumCreateBlockEntities;
 import crazywoddman.warium_create.item.WariumCreateItems;
 
 import com.simibubi.create.foundation.data.CreateRegistrate;
+import com.simibubi.create.foundation.item.ItemDescription;
+import com.simibubi.create.foundation.item.KineticStats;
+import com.simibubi.create.foundation.item.TooltipHelper.Palette;
+import com.simibubi.create.foundation.item.TooltipModifier;
+
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.common.Mod;
@@ -14,6 +19,12 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class WariumCreate {
     public static final String ID = "warium_create";
     public static final CreateRegistrate REGISTRATE = CreateRegistrate.create(ID);
+    static {
+		REGISTRATE.setTooltipModifierFactory(item -> {
+			return new ItemDescription.Modifier(item, Palette.STANDARD_CREATE)
+				.andThen(TooltipModifier.mapNull(KineticStats.create(item)));
+		});
+	}
 
     public WariumCreate() {
         WariumCreateBlocks.REGISTRY.register(FMLJavaModLoadingContext.get().getModEventBus());
