70c1c435569578b417f5fd73baf36ff330cb5b29
CrazyWoddMan <mishaocheret@gmail.com>
2025-09-15 23:38:48 +0200

    propellers mixin patch

--- src/main/java/crazywoddman/warium_create/mixin/valkyrien_warium/PropellersScriptProcedureMixin.java
+++ src/main/java/crazywoddman/warium_create/mixin/valkyrien_warium/PropellersScriptProcedureMixin.java
@@ -1,5 +1,6 @@
 package crazywoddman.warium_create.mixin.valkyrien_warium;
 
+import net.mcreator.valkyrienwarium.block.entity.BoatPropellerTileEntity;
 import net.mcreator.valkyrienwarium.block.entity.VehicleControlNodeBlockEntity;
 import net.mcreator.valkyrienwarium.procedures.HeliRotorScriptProcedure;
 import net.mcreator.valkyrienwarium.procedures.PropellerScriptProcedure;
@@ -30,14 +31,18 @@ public class PropellersScriptProcedureMixin {
         at = @At("HEAD")
     )
     private static void controlledDirection(LevelAccessor world, double x, double y, double z, BlockState blockstate, CallbackInfo ci) {
+
         if (!(world instanceof Level level) || level.isClientSide)
             return;
 
         BlockPos pos = BlockPos.containing(x, y, z);
         BlockEntity blockEntity = level.getBlockEntity(pos);
         CompoundTag data = blockEntity.getPersistentData();
-        double setDirection = level.hasNeighborSignal(pos) ? 1.0 : -1.0;
-        if (blockEntity != null && data.contains("ControlX")) {
+        int setDirection = blockEntity instanceof BoatPropellerTileEntity ? -1 : 1;
+
+        if (level.hasNeighborSignal(pos))
+            setDirection *= -1;
+        else if (blockEntity != null && data.contains("ControlX")) {
             BlockEntity controlNode = blockEntity.getLevel().getBlockEntity(
                 BlockPos.containing(
                     data.getInt("ControlX"),
@@ -47,10 +52,10 @@ public class PropellersScriptProcedureMixin {
             );
 
             if (controlNode != null && controlNode instanceof VehicleControlNodeBlockEntity && controlNode.getPersistentData().getInt("Throttle") < 0)
-                setDirection = 1.0;
+                setDirection *= -1;
         }
 
-        if (data.getDouble("Direction") != setDirection) {
+        if (data.getInt("Direction") != setDirection) {
             data.putDouble("Direction", setDirection);
             blockEntity.setChanged();
         }
